8e50b0c1b63aa7e95cd042bce4f2ce45a7b84b00
devonbahary <devonbahary@gmail.com>
2017-09-29 15:56:44 +0000

    User Story 3: Retrieve User | Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -30,4 +30,22 @@ class Kele
     raise "Invalid credentials" if !@user_auth_token
   end
   
+  
+  # get_me
+  # => retrieves the current user from the Bloc API
+  def get_me
+    # set up a GET request to 'https://www.bloc.io/api/v1/users/me' for current user data
+    headers = {
+      :content_type => 'application/json',
+      :authorization => @user_auth_token
+    }
+    
+    # the GET request
+    path = @bloc_api_url + '/users/me'
+    options = { headers: headers }
+    http_party_response = self.class.get path, options
+    
+    # parse the JSON response to a Ruby hash and return it
+    JSON.parse(http_party_response.body)
+  end
 end
